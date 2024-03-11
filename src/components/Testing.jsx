da277884579179cfa07c857d16ff76ccbe19ffa2
Connor Warme <connor.warme@gmail.com>
2024-03-11 14:44:09 -0600

    Figure out how to get tailwind going (with vite)

--- src/components/Testing.jsx
+++ src/components/Testing.jsx
@@ -2,7 +2,7 @@ const Testing = () => {
   return ( 
     <>
       <div className="h-10 border-2 border-red-500">
-        <h1 className="text-lg">Testing, Testing</h1>
+        <p className="text-lg">Testing, Testing</p>
       </div>
     </>
    );
