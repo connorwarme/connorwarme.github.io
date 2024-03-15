6d25979f8b11495d3b636c5acf1da4a33b9b2eae
Connor Warme <connor.warme@gmail.com>
2024-03-14 20:26:06 -0600

    Build out about section text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,5 @@
-import About from "../components/About";
+const About = () => {
 
-const Home = () => {
-  // about me
   // Hi, I'm Connor - a versatile, hardworking software developer who's committed to building a successful career in software development.
   // I'm proficient in full-stack JavaScript (including React, Next.js, Node.js, Express, and Jest), (maybe include SOLID principles) including testing, debugging, and data handling. 
   // I'm eager for an opportunity to apply and grow my software development knowledge and skills!
@@ -22,48 +20,30 @@ const Home = () => {
   // I also enjoy spending quality time with family, scheming and dreaming about our next adventure rig, and reading.  
   //
   return ( 
-    <>
-      <h1>Connor Warme</h1>
-      <ul>
-        <li>
-          <img />
-          <p>
-            <em>compassionate human</em>
-            {` // a kind and caring presence`}
-          </p>
-        </li>
-        <li>
-          <img />
-          <p>
-            <em>critical thinker</em>
-            {` // a problem-solver`}
-          </p>
-        </li>
-        <li>
-          <img />
-          <p>
-            <em>collaborator</em>
-            {` // empathetic listener and effective communicator`}
-          </p>
-        </li>
-        <li>
-          <img />
-          <p>
-            <em>coder</em>
-            {` // hardworking and detail-driven full-stack developer`}
-          </p>
-        </li>
-        <li>
-          <img />
-          <p>
-            <em>committed to excellence</em>
-            {` // in everything I do`}
-          </p>
-        </li>
-      </ul>
-      <About />
-    </>
+    <div>
+      <h2 className="text-2xl">About</h2>
+      <h3 className="text-lg">Hi, I&#39;m Connor - a versatile, hardworking software developer who&#39;s committed to building a successful career in software development.</h3>
+      <p>
+        I&#39;m proficient in full-stack JavaScript (including React, Next.js, Node.js, Express, and Jest), (maybe include SOLID principles) including testing, debugging, and data handling. 
+        I&#39;m eager for an opportunity to apply and grow my software development knowledge and skills!
+      </p>
+      <p>
+        My educational background prepared me for work as a chaplain or pastor - I have a Masters of Divinity from Princeton Theological Seminary. I&#39;m quite capable, gifted even, but found myself wrestling with fundamental questions, and eventually decided I could no longer be a genuine leader in the faith. 
+        I subsequently learned a new craft and worked as a finish carpenter, installing exquisite millwork in homes worth many millions.
+      </p>
+      <p>
+        I began my coding journey in late 2021. I had always been technologically-inclined; growing up I was always the family&#39;s &quot;resident expert&quot; and troubleshooter extraordinaire. 
+        In my preteen years, I bought a &quot;How to Build a PC for Dummies&quot; book and proceeded to buy all the parts and assemble my own desktop, which served me faithfully for the better part of a decade.  
+        I found The Odin Project, a web development curriculum curated by others who had taught themselves the requisite coding skills to switch careers. It has been as an invaluable resource on my journey, providing a trajectory, learning content, and project ideas to practice along the way. 
+        I&#39;m currently looking for work! You can view my portfolio or contact me.
+      </p>
+      <p>
+        When not at the computer, I&#39;m often supporting (and trying to keep up with) Amity Warme, a professional rock climber. We travel across the western US in search of epic climbing objectives and suitable weather. 
+        I enjoy pursuing mountain adventures (skiing, climbing, biking, trail running).
+        I also enjoy spending quality time with family and friends, scheming and dreaming about our next adventure rig, and reading. 
+      </p>
+    </div>
    );
 }
  
-export default Home;
+export default About;
