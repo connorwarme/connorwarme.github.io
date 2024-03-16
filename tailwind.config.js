36c6c8f2e271af9addf7f27d41c9325a38b97766
Connor Warme <connor.warme@gmail.com>
2024-03-16 14:27:39 -0600

    Add palette plus of colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,24 @@ export default {
     "./src/**/*.{html,jsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'wave-spray': '#d2e6ee',
+        'light-blue': '#b5d4df',
+        'glacier': '#afd2e0',
+        'sky-blue': '#8abdcf',
+        'bay-blue': '#72b0c3',
+        'moonstone': '#62a4b5',
+        'bay-current': '#3b7681',
+        'gunmetal': '#0f242d',
+        
+        'tree-green': '#255843',
+        'myrtle-green': '#417176',
+        'dark-forest': '#113932',
+        'deep-forest': '#1b3a35',
+        'midnight-green': '#1f434a',
+      },
+    },
   },
   plugins: [],
 };
