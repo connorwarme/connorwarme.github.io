d53b30df395cbb0e215cfe99246da9c21ecd20c7
Connor Warme <connor.warme@gmail.com>
2024-03-18 16:54:07 -0600

    Build mock up of portolio item data

---
+++ src/data/projectData.js
@@ -0,0 +1,47 @@
+import proHome from "../images/gallery/screenshots/pro-home.png"
+import reactIcon from "../images/icons/react.svg"
+
+const projectData = [
+  {
+    title: 'Amity Warme',
+    img: {
+      src: proHome,
+      alt: 'Amity home page',
+    },
+    url: 'https://amitywarme.com',
+    repo_url: 'https://github.com/connorwarme/pro-site',
+    points: ['Establish online presence', 'Includes gallery, blog, contact, and more'],
+    tools: [
+      {
+        title: 'React',
+        img: {
+          src: reactIcon,
+          alt: 'React icon',
+        }
+      },
+      {
+        title: 'React',
+        img: {
+          src: reactIcon,
+          alt: 'React icon',
+        }
+      },
+      {
+        title: 'React',
+        img: {
+          src: reactIcon,
+          alt: 'React icon',
+        }
+      },
+      {
+        title: 'React',
+        img: {
+          src: reactIcon,
+          alt: 'React icon',
+        }
+      },
+    ]
+  }
+]
+
+export default projectData
