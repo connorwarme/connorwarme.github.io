77b0f288ce715070f75fc3e3d00c94dd514c1fa5
Connor Warme <connor.warme@gmail.com>
2024-03-18 21:39:29 -0600

    Adjust to account for different styles

--- src/components/ToolUnit.jsx
+++ src/components/ToolUnit.jsx
@@ -1,10 +1,12 @@
 const ToolUnit = ({ unit }) => {
+
+  const imgClass = !unit.mini ? "w-14 h-14 m-2" : "w-6 h-6 m-2"
   return ( 
     <div className="flex flex-col justify-center items-center">
       { unit && unit.img && unit.img.src && (
         <>
-          <img src={unit.img.src} alt={unit.img.alt} className="w-14 h-14 m-2" />
-          <p className="text-sm">{unit.title}</p>
+          <img src={unit.img.src} alt={unit.img.alt} className={imgClass} />
+          { !unit.mini && <p className="text-sm">{unit.title}</p> }
         </>
       )}
     </div>
