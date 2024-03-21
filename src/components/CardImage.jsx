a832121cfc6f5f3a3e7eb486f8ffa0812899e1e7
Connor Warme <connor.warme@gmail.com>
2024-03-21 13:26:36 -0600

    Run prettier

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -1,14 +1,27 @@
 const CardImage = ({ item }) => {
-  return ( 
-    <figure className="group w-inherit relative overflow-hidden rounded-md">
-      <img src={item.img.src} alt={item.img.alt} className="w-full h-full object-cover transition ease-in-out duration-250 group-hover:scale-110" />
-      <figcaption className="flex flex-col justify-evenly items-center p-2 z-20 absolute top-0 left-0 h-full w-full opacity-0 group-hover:opacity-100 font-play transition ease-in-out duration-300" >
-        <h2 className="text-xl text-wave-spray font-bold text-center" >{item.summary}</h2>
-        <a className="border-moonstone text-wave-spray border-2 text-center w-fit p-2 pr-4 pl-4 rounded-lg hover:bg-moonstone transition ease-in-out duration-300 font-semibold" href={item.url} target="_blank" rel="noreferrer">Visit Website</a>
+  return (
+    <figure className="w-inherit group relative overflow-hidden rounded-md">
+      <img
+        src={item.img.src}
+        alt={item.img.alt}
+        className="duration-250 h-full w-full object-cover transition ease-in-out group-hover:scale-110"
+      />
+      <figcaption className="absolute left-0 top-0 z-20 flex h-full w-full flex-col items-center justify-evenly p-2 font-play opacity-0 transition duration-300 ease-in-out group-hover:opacity-100">
+        <h2 className="text-center text-xl font-bold text-wave-spray">
+          {item.summary}
+        </h2>
+        <a
+          className="w-fit rounded-lg border-2 border-moonstone p-2 pl-4 pr-4 text-center font-semibold text-wave-spray transition duration-300 ease-in-out hover:bg-moonstone"
+          href={item.url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Visit Website
+        </a>
       </figcaption>
-      <div className="bg-gradient-to-r from-bay-current via-slate-800 to-dark-forest z-10 absolute top-0 left-0 h-full w-full opacity-0 group-hover:opacity-100 rounded-md transition ease-in-out duration-500" ></div>
+      <div className="absolute left-0 top-0 z-10 h-full w-full rounded-md bg-gradient-to-r from-bay-current via-slate-800 to-midnight-green opacity-0 transition duration-500 ease-in-out group-hover:opacity-100"></div>
     </figure>
-   );
+  )
 }
- 
-export default CardImage;
+
+export default CardImage
