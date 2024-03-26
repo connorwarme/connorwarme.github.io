492ef0ca0baeecc6f7fb542ec1af74ef2df1c1b1
Connor Warme <connor.warme@gmail.com>
2024-03-26 16:51:30 -0600

    Create external link component

---
+++ src/components/ExternalLink.jsx
@@ -0,0 +1,13 @@
+import { HiOutlineExternalLink as LinkIcon } from 'react-icons/hi'
+const ExternalLink = ({ link }) => {
+
+  const style = `inline-block hover:underline ${link.style}`
+  return ( 
+    <a href={link.url} target="_blank" rel="noreferrer" className={style}>
+      <span>{link.text}</span>
+      <LinkIcon className='h-4 w-4 flex-shrink-0 ml-[3px] mb-[2px] inline-block'/>
+    </a>
+   );
+}
+ 
+export default ExternalLink;
