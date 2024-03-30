12c185e6779fcf219d12b7e8ecd56d62655d491e
Connor Warme <connor.warme@gmail.com>
2024-03-30 16:06:54 -0600

    Think through scenarios (pending, error, success) and implement

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import { MdOutlineErrorOutline as ErrorIcon } from "react-icons/md";
 
 const Contact = () => {
   const [email, setEmail] = useState(null)
@@ -7,7 +9,8 @@ const Contact = () => {
   const [messageError, setMessageError] = useState(null)
   const [pending, setPending] = useState(null)
   const [success, setSuccess] = useState(null)
-  const [error, setError] = useState(null)
+  const [error, setError] = useState('Apologies! There was an error sending your message. Please refresh the page to try again, or email amity@amitywarme.com. Sorry for the inconvenience!')
+  const [response, setResponse] = useState(null)
 
   const handleEmail = (e) => {
     if (e.target.value.length === 0) {
@@ -46,9 +49,10 @@ const Contact = () => {
     .then(data => {
       if (data && data.errors) {
         setSuccess(false)
-        setError('Apologies! There was an error sending your message. Please refresh the page to try again, or email amity@amitywarme.com. Sorry for the inconvenience!')
+        setError(true)      
       } else {
         setSuccess(true)
+        setResponse(data)
       }
       setPending(false);
 
@@ -58,7 +62,7 @@ const Contact = () => {
       console.log(e)
       setSuccess(false)
       setPending(false)
-      setError('Apologies! There was an error sending your message. Please refresh the page to try again, or email amity@amitywarme.com. Sorry for the inconvenience!');
+      setError(true)
     })
   }
 
@@ -67,25 +71,44 @@ const Contact = () => {
     <div className="w-dvw">
       <h1 className="text-4xl font-grenze text-center m-10">Contact</h1>
       <section className="w-full flex flex-col py-8 bg-wave-spray/75 text-midnight-green p-4 mx-auto">
-        <form onSubmit={handleSubmit} className="font-play w-full" action="#" autoComplete="off">
-          <div className="flex flex-col space-y-4 lg:space-y-3 mb-6 font-semibold">
-            <div className="pb-4 text-center">
-              <h3 className="text-lg font-bold">I&apos;d love to hear from you!</h3>
+        { !success && (
+          <form onSubmit={handleSubmit} className="font-play w-full" action="#" autoComplete="off">
+            <div className="flex flex-col space-y-4 lg:space-y-3 mb-6 font-semibold">
+              <div className="pb-4 text-center">
+                <h3 className="text-lg font-bold">I&apos;d love to hear from you!</h3>
+              </div>
+              <div>
+                <input type="email" id="email" name="email" placeholder="Your email" className="w-full placeholder-midnight-green bg-transparent border-2 border-midnight-green py-2 px-4 text-sm lg:text-base outline-midnight-green focus:outline-2 focus:bg-wave-spray disabled:opacity-50 disabled:cursor-not-allowed rounded-sm" onChange={handleEmail} />
+                { emailError && <span className="text-sm text-red-600" >{emailError}</span> }
+              </div>
+              <div>
+                <textarea id="message" name="message" placeholder="Your message" rows="10" className="w-full placeholder-midnight-green bg-transparent border-2 border-midnight-green py-3 px-4 text-sm lg:text-base outline-midnight-green focus:outline-2 focus:bg-wave-spray disabled:opacity-50 disabled:cursor-not-allowed rounded-sm" onChange={handleMessage}/>
+                { messageError && <span className="text-sm text-red-600">{messageError}</span> }
+              </div>
             </div>
-            <div>
-              <input type="email" id="email" name="email" placeholder="Your email" className="w-full placeholder-midnight-green bg-transparent border-2 border-midnight-green py-2 px-4 text-sm lg:text-base outline-midnight-green focus:outline-2 focus:bg-wave-spray disabled:opacity-50 disabled:cursor-not-allowed rounded-sm" onChange={handleEmail} />
-              { emailError && <span className="text-sm text-red-600" >{emailError}</span> }
-            </div>
-            <div>
-              <textarea id="message" name="message" placeholder="Your message" rows="10" className="w-full placeholder-midnight-green bg-transparent border-2 border-midnight-green py-3 px-4 text-sm lg:text-base outline-midnight-green focus:outline-2 focus:bg-wave-spray disabled:opacity-50 disabled:cursor-not-allowed rounded-sm" onChange={handleMessage}/>
-              { messageError && <span className="text-sm text-red-600">{messageError}</span> }
-            </div>
+            <div className="flex justify-end">
+              { !pending 
+              ? <button type="submit" className="text-lg bg-transparent text-midnight-green font-semibold border-2 border-midnight-green rounded-lg py-2 px-4 mr-4 hover:bg-wave-spray">Send</button> 
+              : <button type="submit" disabled className="text-lg bg-transparent text-midnight-green font-semibold border-2 border-midnight-green rounded-lg py-2 px-4 mr-4 hover:bg-wave-spray">Sending</button>}
+            </div> 
+            { error && (
+              <div className="my-4">
+                <div className="flex items-center">
+                  <ErrorIcon className="text-red-600 h-8 w-8 flex-shrink-0 m-2"/>
+                  <p className="font-semibold">Apologies! There was an error sending your message.</p>
+                </div>
+                <p>Please refresh the page to try again, or email amity@amitywarme.com. Sorry for the inconvenience!</p>
+              </div>
+            )}
+          </form>
+        )}
+        { success && (
+          <div>
+            <h3>Thanks for reaching out!</h3>
+            <p>I&apos;ll get back to you shortly, with my reply going to {response.contact.email}.</p>
+            <Link to={"/"}>Return to Home</Link>
           </div>
-          <div className="flex justify-end">
-            <button type="submit" className="text-lg bg-transparent text-midnight-green font-semibold border-2 border-midnight-green rounded-lg py-2 px-4 mr-4 hover:bg-wave-spray">Send</button>
-          </div>
-          
-        </form>
+        )}
       </section>
     </div>
    );
