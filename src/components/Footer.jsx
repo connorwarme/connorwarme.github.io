ef0b4e2560055b4445621ce0e7e943a493332407
Connor Warme <connor.warme@gmail.com>
2024-04-02 10:50:13 -0600

    Continue creating and styling footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,28 @@
-import { VscTools } from 'react-icons/vsc'
+import { VscTools, VscGithubInverted } from 'react-icons/vsc'
+import { BsLinkedin } from "react-icons/bs";
 import { siteTools } from '../data/toolData';
 import ToolUnit from './ToolUnit';
 
 const Footer = () => {
 
   return ( 
-    <div className="w-dvw">
-      <a href="#top">Back to Top</a>
-      <div>
-        <p>Github</p>
-        <p>LinkedIn</p>
+    <div className="w-dvw bg-midnight-green/75 text-wave-spray font-play flex flex-col justify-center p-4">
+      <a href="#top" className="text-center font-body ">Back to Top</a>
+      <div className='flex justify-center py-2'>
+        <div>
+          <a href="https://github.com/connorwarme" target='_blank' rel='noreferrer' title='Check out my GitHub profile' className='hover:scale-110'>
+            <VscGithubInverted className='m-2 h-6 w-6 hover:rotate-[360deg] transition-all ease-in-out duration-500' aria-label='GitHub icon'/>
+          </a>
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/in/connor-warme-13c8" target='_blank' rel='noreferrer' title='Connect on LinkedIn'>
+            <BsLinkedin alt="LinkedIn" className='m-2 h-6 w-6 hover:rotate-[360deg] transition-all ease-in-out duration-500' aria-label='LinkedIn icon' />
+          </a>
+        </div>
       </div>
-      <div className='flex justify-center items-center'>
+      <div className='flex justify-center items-center border-t border-wave-spray pt-2'>
         <span className='flex justify-center'>
-          <VscTools className="h-6 w-6 text-wave-spray" />
+          <VscTools className="h-6 w-6 " />
           <span className="ml-1">:</span>
         </span>
         <div className='flex'>
