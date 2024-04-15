92eda23a3a0e16a7f2bac66d4bb17bf3b38c62d1
Connor Warme <connor.warme@gmail.com>
2024-04-15 11:09:06 -0600

    Play with different display views at wider screen

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,55 +10,57 @@ const Home = () => {
         Connor Warme
       </h1>
       <div className='grid sm:grid-cols-6'>
-      <div className='col-start-2 col-end-6 md:bg-midnight-green/55'>
-      <div className="mb-4 mt-4 flex justify-center">
-        <img
-          src={portraitPhoto}
-          alt="Portrait of Connor"
-          className="h-40 w-48 rounded-lg border border-wave-spray/50 object-cover sm:w-96 sm:h-80"
-        />
-      </div>
-      <div className='m-2 mb-40 flex justify-center'>
-        <ul className="grid justify-center gap-3 rounded-md bg-midnight-green/75 pb-3 text-start font-body text-wave-spray w-full max-w-sm">
-          <li>
-            <img />
-            <p className="text-xl">
-              <em className="font-grenze text-3xl font-semibold">C</em>
-              ompassionate human
-            </p>
-            <p>{` // a kind and caring presence`}</p>
-          </li>
-          <li>
-            <img />
-            <p className="text-xl">
-              <em className="font-grenze text-3xl font-semibold">C</em>ritical
-              thinker
-            </p>
-            <p>{` // a detail-driven problem-solver`}</p>
-          </li>
-          <li>
-            <img />
-            <p className="text-xl">
-              <em className="font-grenze text-3xl font-semibold">C</em>ommunicator
-            </p>
-            <p>{` // empathetic listener and collaborator`}</p>
-          </li>
-          <li>
-            <img />
-            <p className="text-xl">
-              <em className="font-grenze text-3xl font-semibold">C</em>oder
-            </p>
-            <p>{` // versatile full-stack developer`}</p>
-          </li>
-          <li>
-            <img />
-            <p className="text-xl">
-              <em className="font-grenze text-3xl font-semibold">C</em>ommitted to
-              excellence
-            </p>
-            <p>{` // in everything I do`}</p>
-          </li>
-        </ul>
+      <div className='sm:col-start-2 sm:col-end-6 lg:bg-midnight-green'>
+      <div className='md:grid lg:grid-cols-2 lg:bg-midnight-green'>
+        <div className="mb-4 mt-4 flex justify-center">
+          <img
+            src={portraitPhoto}
+            alt="Portrait of Connor"
+            className="h-40 w-48 rounded-lg border border-wave-spray/50 object-cover sm:w-96 sm:h-96"
+          />
+        </div>
+        <div className='m-2 mb-40 flex justify-center lg:justify-start lg:mb-2'>
+          <ul className="grid justify-center gap-3 rounded-md bg-midnight-green/75 pb-3 text-start font-body text-wave-spray w-full max-w-sm lg:bg-transparent">
+            <li>
+              <img />
+              <p className="text-xl">
+                <em className="font-grenze text-3xl font-semibold">C</em>
+                ompassionate human
+              </p>
+              <p>{` // a kind and caring presence`}</p>
+            </li>
+            <li>
+              <img />
+              <p className="text-xl">
+                <em className="font-grenze text-3xl font-semibold">C</em>ritical
+                thinker
+              </p>
+              <p>{` // a detail-driven problem-solver`}</p>
+            </li>
+            <li>
+              <img />
+              <p className="text-xl">
+                <em className="font-grenze text-3xl font-semibold">C</em>ommunicator
+              </p>
+              <p>{` // empathetic listener and collaborator`}</p>
+            </li>
+            <li>
+              <img />
+              <p className="text-xl">
+                <em className="font-grenze text-3xl font-semibold">C</em>oder
+              </p>
+              <p>{` // versatile full-stack developer`}</p>
+            </li>
+            <li>
+              <img />
+              <p className="text-xl">
+                <em className="font-grenze text-3xl font-semibold">C</em>ommitted to
+                excellence
+              </p>
+              <p>{` // in everything I do`}</p>
+            </li>
+          </ul>
+        </div>
       </div>
       <Porfolio />
       <AboutMini />
